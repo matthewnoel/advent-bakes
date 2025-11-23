5a0ba0a37a6d62101dcab63b234e9e3283d20bf3
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2025-11-23 19:54:56 +0000

    lemon iced

--- src/lib/recipes.ts
+++ src/lib/recipes.ts
@@ -23,7 +23,7 @@ export type Countable = {
 	quantity: number;
 };
 
-export type Ingredient = Mass | Volume | Countable;
+export type Ingredient = Mass | Volume | Countable; // TODO: should ingredients have an optional recommendation string? E.g. fine salt when it needs to be sifted
 
 export const ChocolateEdgedChocolateChipCookies: Recipe = {
 	title: 'Chocolate-Edged Chocolate Chip Cookies',
@@ -104,11 +104,52 @@ export const DoubleChocolateChewies: Recipe = {
 	estimatedCount: 32
 };
 
+// c.a.b
 export const LemonIcedCookies: Recipe = {
 	title: 'Lemon Iced Cookies',
-	ingredients: [],
-	instructions: [],
-	estimatedCount: 0
+	ingredients: [
+		{
+			type: 'mass',
+			value: 'all purpose flour',
+			grams: 420
+		},
+		// TODO: 1/2 tsp baking soda to mass
+		// TODO: 1/2 tsp salt to mass
+		{
+			type: 'mass',
+			value: 'room temp unsalted butter',
+			grams: 110
+		},
+		{
+			type: 'mass',
+			value: 'granulated sugar',
+			grams: 150
+		},
+		{
+			type: 'countable',
+			value: 'egg',
+			quantity: 1
+		},
+		{
+			type: 'volume',
+			value: 'fresh lemon juice',
+			milliliters: 50
+		}
+		// TODO: optional jelly bean garnish?
+	],
+	instructions: [
+		'Preheat the oven to 375F',
+		'Whisk together flour, baking soda, and salt',
+		'Beat the butter and sugar on medium for about 2 minutes until light',
+		'Beat in the egg and lemon juice',
+		'Gradually beat in the flour on low. Stop once mixed and the dough is smooth but slightly stiff.',
+		'Portion dough onto baking sheets and flatten tops with a greased glass',
+		'Bake for approximately 10 minutes until firm and golden brown',
+		'Rest cookies before icing',
+		'Make the icing by sifting the powdered sugar and whisking in lemon juice until smooth',
+		'Spread icing over each cookie and let the icing dry'
+	],
+	estimatedCount: 34
 };
 
 export const MaltedMilkChocolateCookies: Recipe = {
