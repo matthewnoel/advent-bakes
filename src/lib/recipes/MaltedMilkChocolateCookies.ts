48691f8f63799822cf77c800e3a38cd8065b1cf1
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2025-12-04 00:53:25 +0000

    started malted milk chocolate

--- src/lib/recipes/MaltedMilkChocolateCookies.ts
+++ src/lib/recipes/MaltedMilkChocolateCookies.ts
@@ -1,8 +1,58 @@
-import type { Recipe } from './types';
+import { Sources, type Recipe } from './types';
 
 export const MaltedMilkChocolateCookies: Recipe = {
 	title: 'Malted Milk Chocolate Cookies',
-	ingredients: [],
+	ingredients: [
+		{
+			type: 'mass',
+			value: 'unsalted room temperature butter',
+			grams: 220
+		},
+		{
+			type: 'mass',
+			value: 'sugar',
+			grams: 100
+		},
+		{
+			type: 'countable',
+			value: 'egg',
+			quantity: 1
+		},
+		{
+			type: 'volume',
+			value: 'vanilla extract',
+			milliliters: 7
+		},
+		{
+			type: 'mass',
+			value: 'plain malted milk powder',
+			grams: 105
+		},
+		{
+			type: 'mass',
+			value: 'salt',
+			grams: 3
+		},
+		{
+			type: 'mass',
+			value: 'all-purpose flour',
+			grams: 280
+		},
+		{
+			type: 'mass',
+			value: 'milk chocolate chips',
+			grams: 230
+		},
+		{
+			type: 'volume',
+			value: 'grape seed oil (flavorless vegetable oil)',
+			milliliters: 5
+		}
+	],
 	instructions: [],
-	estimatedCount: 0
+	estimatedCount: 48,
+	source: {
+		value: Sources.CookieAdventBook
+	},
+	isComplete: true
 };
