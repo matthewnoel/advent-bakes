fbd6828d7784d28f12822336a85f3a2647e412a5
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2025-12-09 02:30:17 +0000

    partial crinkle

--- src/lib/recipes/CrinkleCookies.ts
+++ src/lib/recipes/CrinkleCookies.ts
@@ -1,8 +1,58 @@
-import type { Recipe } from './types';
+import { Sources, type Recipe } from './types';
 
 export const CrinkleCookies: Recipe = {
 	title: 'Crinkle Cookies',
-	ingredients: [],
-	instructions: [],
-	estimatedCount: 0
+	ingredients: [
+		{
+			type: 'mass',
+			value: 'unsweetened baking chocolate',
+			grams: 115
+		},
+		{
+			type: 'mass',
+			value: 'unsalted butter',
+			grams: 55
+		},
+		{
+			type: 'mass',
+			value: 'all purpose flour',
+			grams: 280
+		},
+		{
+			type: 'mass',
+			value: 'sugar',
+			grams: 400
+		},
+		{
+			type: 'countable',
+			value: 'eggs',
+			quantity: 4
+		},
+		{
+			type: 'mass',
+			value: 'baking powder',
+			grams: 7
+		},
+		{
+			type: 'volume',
+			value: 'ground cinnamon',
+			milliliters: 1
+		},
+		{
+			type: 'mass',
+			value: 'sifted powdered sugar',
+			grams: 125
+		}
+	],
+	instructions: [
+		'Chop chocolate and chunk butter. Melt chocolate and butter together then let cool.',
+		'Combine cooled chocolate mixture, half of the flour, granulated sugar, and eggs then beat together on medium until combined (approximately 2 minutes).',
+		''
+	],
+	estimatedCount: 42,
+	source: {
+		value: Sources.CookieAdventBook,
+		isModified: true
+	},
+	isComplete: true
 };
