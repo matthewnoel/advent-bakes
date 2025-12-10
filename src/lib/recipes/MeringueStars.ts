94f7a0dc9506f653918c55e571308ef4b2af8a9b
Matthew Noel <31493274+matthewnoel@users.noreply.github.com>
2025-12-09 21:47:15 -0800

    stars (#26)

--- src/lib/recipes/MeringueStars.ts
+++ src/lib/recipes/MeringueStars.ts
@@ -1,8 +1,49 @@
-import type { Recipe } from './types';
+import { Sources, type Recipe } from './types';
 
 export const MeringueStars: Recipe = {
 	title: 'Meringue Stars',
-	ingredients: [],
-	instructions: [],
-	estimatedCount: 0
+	ingredients: [
+		{
+			type: 'countable',
+			value: 'room temperature egg whites',
+			quantity: 5
+		},
+		{
+			type: 'volume',
+			value: 'salt',
+			milliliters: 1
+		},
+		{
+			type: 'mass',
+			value: 'sugar',
+			grams: 250
+		},
+		{
+			type: 'volume',
+			value: 'vanilla extract',
+			milliliters: 5
+		},
+		{
+			type: 'volume',
+			value: 'almond extract',
+			milliliters: 2.5
+		},
+		{
+			type: 'countable',
+			value: 'can of edible glitter spray',
+			quantity: 1
+		}
+	],
+	instructions: [
+		'Preheat the oven to 225° F.',
+		'On high, beat the egg whites and salt until foamy. Gradually beat in the sugar, then beat until the meringue hold stiff (approximately 3 minutes). Finally beat in the vanilla and almond extracts until combined.',
+		'Pipe the meringue via a pastry bag onto sheets lined with aluminum foil, then spray with edible glitter and sprinkle with sugar.',
+		'Bake for 30 minutes, then turn off the oven without opening the door and leave overnight.'
+	],
+	estimatedCount: 130,
+	source: {
+		value: Sources.CookieAdventBook,
+		isModified: true
+	},
+	isPublished: true
 };
